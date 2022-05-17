a9dfb9c749378f8009171ec84a8466d7ff012344
Same Mahmoud <samemahmoud24@gmail.com>
2022-05-17 11:52:41 +0200

    Added the Task model

--- db/migrate/20220517094712_create_tasks.rb
+++ db/migrate/20220517094712_create_tasks.rb
@@ -3,9 +3,7 @@ class CreateTasks < ActiveRecord::Migration[6.1]
     create_table :tasks do |t|
       t.string :title
       t.text :details
-      t.boolean :completed
-      t.false :default
-
+      t.boolean :completed, default: false
       t.timestamps
     end
   end
